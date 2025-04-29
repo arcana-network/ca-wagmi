6cb6c7b57d2989ef7de27cb039191ad9fa51b7df
makylfang <makyl@newfang.io>
2025-04-29 16:07:51 +0400

    fix: added getSupportedChains fn and added ready boolean for ca init

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ import { useWriteContract } from "./hooks/useWriteContract";
 import { useCAFn } from "./hooks/useCA";
 import { CAUnifiedBalanceContext } from "./context";
 import type { UseBalanceReturnValue } from "./hooks/useUnifiedBalance";
-import { Network } from "@arcana/ca-sdk";
+import { Network, CA } from "@arcana/ca-sdk";
+
+const getSupportedChains = CA.getSupportedChains;
 
 export {
   Network,
@@ -23,6 +25,7 @@ export {
   useUnifiedBalance,
   CAProvider,
   CAUnifiedBalanceContext,
+  getSupportedChains,
 };
 
 export type { UseBalanceReturnValue as UseBalanceValue };
